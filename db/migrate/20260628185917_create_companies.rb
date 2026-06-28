38cad7d2e4cf3e00c162a73df6ca6b729c15783e
railsonn <railsongamer99@gmail.com>
2026-06-28 16:01:09 -0300

    initial commit

---
+++ db/migrate/20260628185917_create_companies.rb
@@ -0,0 +1,10 @@
+class CreateCompanies < ActiveRecord::Migration[8.0]
+  def change
+    create_table :companies do |t|
+      t.integer :cnpj
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
