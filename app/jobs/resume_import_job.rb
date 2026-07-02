a53ae44afdd3021325b3c626c50c49c186e9800e
railsonn <railsongamer99@gmail.com>
2026-07-02 15:52:57 -0300

    fix: modify job resume

--- app/jobs/resume_import_job.rb
+++ app/jobs/resume_import_job.rb
@@ -1,7 +1,10 @@
 class ResumeImportJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(resume_id)
+    resume = Resume.find(resume_id)
+    resume.import!
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.error("Resume with ID #{resume_id} not found: #{e.message}")
   end
 end
