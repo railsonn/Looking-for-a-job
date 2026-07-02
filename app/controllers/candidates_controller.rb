f4e3f82239e66844a23be5fde2265f60520ba269
railsonn <railsongamer99@gmail.com>
2026-07-01 21:13:11 -0300

    fix: change candidate controllers

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -21,8 +21,22 @@ class CandidatesController < ApplicationController
   end
 
   def delete
+    @candidate = Candidate.find(params[:id])
+
+    if @candidate.destroy
+      redirect_to candidates_path, notice: 'Candidate was successfully deleted.'
+    else
+      redirect_to candidates_path, alert: 'Failed to delete candidate.'
+    end
   end
 
   def create
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      redirect_to candidates_path, notice: 'Candidate was successfully created.'
+    else
+      render :new
+    end
   end
 end
