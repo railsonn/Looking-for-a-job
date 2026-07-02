47f9ca4555015ffd10c4c2b3ceed584b13521139
railsonn <railsongamer99@gmail.com>
2026-07-01 21:17:27 -0300

    fix: change company controllers

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,19 +1,39 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.all
   end
 
   def new
   end
 
   def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      redirect_to companies_path, notice: 'Company was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      redirect_to companies_path, notice: 'Company was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def delete
+    @company = Company.find(params[:id])
+    if @company.destroy
+      redirect_to companies_path, notice: 'Company was successfully deleted.'
+    else
+      redirect_to companies_path, alert: 'Failed to delete company.'
+    end
   end
 end
