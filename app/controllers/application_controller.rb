58519f017a1f2f85577937a7f9de760dc7580c76
railsonn <railsongamer99@gmail.com>
2026-07-02 16:22:41 -0300

    feat: add current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  before_action :authenticate_user!
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
   allow_browser versions: :modern
 end
