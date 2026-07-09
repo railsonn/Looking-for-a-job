5e3843bf03b270346ef4d84f75fefeeedab2c72c
railsonn <railsongamer99@gmail.com>
2026-07-09 16:20:54 -0300

    feat: add configs in controller

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,8 +1,33 @@
 class ResumesController < ApplicationController
+  require "pdf/reader"
+
   def index
   end
 
   def new
+    @resume = Resume.new
+  end
+
+  def create
+    @resume = Resume.new(resume_params)
+    
+    if @resume.save
+      ResumeImportJob.perform_later(@resume.id)
+      redirect_to resumes_path, notice: 'Resume was successfully uploaded and is being processed.'
+    else
+      render :new
+    end
+
+    # Extrair texto do PDF
+    pdf = PDF::Reader.new(
+      StringIO.new(resume.file.download)
+    )
+
+    text = pdf.pages.map(&:text).join("\n")
+
+    puts "Texto extraído do PDF: #{text} ----------------------------------------"
+
+    @resumes = Resume.all
   end
 
   def edit
@@ -15,5 +40,11 @@ class ResumesController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(:file)
   end
 end
