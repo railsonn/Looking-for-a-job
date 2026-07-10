e349efb18ba092e3e504b374d03b1b4200755d94
railsonn <railsongamer99@gmail.com>
2026-07-10 15:59:30 -0300

    fix: remove validates

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,9 +3,7 @@ class Job < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
-  validates :requirements, presence: true
   validates :salary, presence: true
   validates :location, presence: true
-  validates :employment_type, presence: true
-  validates :status, presence: true
+  validates :contract_type, presence: true
 end
--- db/migrate/20260710184033_add_company_to_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyToJobs < ActiveRecord::Migration[8.0]
-  def change
-    add_reference :jobs, :company, null: false, foreign_key: true
-  end
-end
