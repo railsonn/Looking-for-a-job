1d888cc86cb04a7a0f53a16da25801e5b72d8ee4
railsonn <railsongamer99@gmail.com>
2026-07-10 15:37:45 -0300

    feat: add validates in job and add references in model company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ApplicationRecord
+  has_many :jobs, dependent: :destroy
+
+  has_one_attached :logo
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,11 @@
 class Job < ApplicationRecord
-  
+  belongs_to :company
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :requirements, presence: true
+  validates :salary, presence: true
+  validates :location, presence: true
+  validates :employment_type, presence: true
+  validates :status, presence: true
 end
