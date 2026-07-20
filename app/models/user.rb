f629c2ec132ff942968838dcfd353787d25514b6
railsonn <railsongamer99@gmail.com>
2026-07-20 13:01:59 -0300

    feat: add validates in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   has_one :company, dependent: :destroy
   has_one :candidate, dependent: :destroy
+
+  validates :email, presence: true
 end
